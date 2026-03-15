63a88e3033b56638c23cb87176d9981a92ef054d
character-r <riblolox@gmail.com>
2026-03-15 00:12:25 -0700

    Update code.lua

--- code.lua
+++ code.lua
@@ -7,4 +7,8 @@ function mod.SELite(player)
   require(128810736268190)(player)
 end
 
+function mod.Sensation(player)
+  require(100263845596551)(player.Name, ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")
+end
+
 return mod
